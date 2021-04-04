f88285c021e9add5e0a0f8f8303e4dfedce3a369
Max <max_lisyanskiy@mail.ru>
2021-04-04 20:26:15 +0300

    first commit

---
+++ components/system/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="Header">
+      <header>
+        <div class="navbar">
+          <div class="container">
+            <div class="navbar-content">
+              <div class="logo">Blog</div>
+              <ul class="navbar-list">
+                <li class="navbar-item" v-for="link in links" :key="link.title">
+                  <nuxt-link class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</nuxt-link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        components: {},
+        data() {
+            return {
+              links: [
+                { title: 'Home', url: '/' }
+              ]
+            }
+        },
+        methods: {}
+    }
+</script>
+
+<style lang="scss">
+
+</style>
